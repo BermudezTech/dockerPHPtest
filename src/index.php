d3c0eee29954040cb1e8250d6fcb2e02b00f503e
JBermudezG <jebermudez587@gmail.com>
2023-11-20 12:28:19 -0500

    Primer commit

---
+++ src/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Docker & PHP</title>
+</head>
+
+<body>
+    <h1><?php echo "Hello World from PHP with Docker"; ?>
+    </h1>
+    <p>Here is the PHP version: <?php echo phpversion(); ?></p>
+</body>
+
+</html>
