d6e16510d6e84dd53748e5c70987582e48e7b3fc
José Eduardo Bermúdez Garavito <51098132+BermudezTech@users.noreply.github.com>
2023-12-08 21:27:34 -0500

    Update index.php

--- src/index.php
+++ src/index.php
@@ -8,7 +8,7 @@
 </head>
 
 <body>
-    <h1><?php echo "Hello World from PHP with Docker"; ?>
+    <h1><?php echo "Hello World from PHP with Docker and Github Actions"; ?>
     </h1>
     <p>Here is the PHP version: <?php echo phpversion(); ?></p>
 </body>
